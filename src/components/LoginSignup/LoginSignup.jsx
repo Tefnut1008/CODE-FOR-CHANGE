8637e2bb263d2deef92e07e7bf4db6ecaeeb639e
ClimateOP <baburaogaonkar906@gmail.com>
2025-03-16 01:35:49 +0530

    add login options

--- src/components/LoginSignup/LoginSignup.jsx
+++ src/components/LoginSignup/LoginSignup.jsx
@@ -1,39 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginSignup.css'
 import { FaUser, FaLock } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 
 const LoginSignup = () => {
-
-  // const [action,setAction] = usestate("Sign Up");
+  
+  const [choice, setChoice] = useState("Patient")
+  const [action, setAction] = useState("Sign Up");
 
   return (
     <div className='wrapper'>
-        <form action="">
-          <h1>Login</h1>
-          <div className='input-box'>
-            <input type='email' placeholder='Gmail' required />
-            <MdEmail className='icon'/>
-          </div>
-          <div className='input-box'>
-            <input type='text' placeholder='Username' required />
-            <FaUser className='icon'/>
-          </div>
-          <div className='input-box'>
-            <input type='password' placeholder='Password' required />
-            <FaLock className='icon'/>
-          </div>
-          <div className='remember-forgot'>
-            <label><input type="checkbox" />Remember me</label>
-            <a href='#'>Forgot password?</a>
-          </div>
-          <button type='submit'>Login</button>
-          <div className="register-link">
-            <p>Don't have an account?
-              <a href='#'>Sign Up</a>
-            </p>
-          </div>
-        </form>
+      <form action="">
+      <button id='patient' className={choice=='Patient'?"filled":"empty"} type='button' onClick={()=>{setChoice('Patient')}}>Patient</button>
+      <button id='doctor' className={choice=='Patient'?"empty":"filled"} type='button' onClick={()=>{setChoice('Doctor')}}>Doctor</button>
+        <div className='window'>{action === 'Sign Up' ? "Log in" : "Sign Up"}</div>
+        {action === 'Sign Up' ? <div></div> : <div className='input-box'>
+          <input type='email' placeholder='Gmail' required />
+          <MdEmail className='icon' />
+        </div>}
+
+        <div className='input-box'>
+          <input type='text' placeholder='Username' required />
+          <FaUser className='icon' />
+        </div>
+        <div className='input-box'>
+          <input type='password' placeholder='Password' required />
+          <FaLock className='icon' />
+        </div>
+        <div className='remember-forgot'>
+          <label><input type="checkbox" />Remember me</label>
+          <a href='#'>Forgot password?</a>
+        </div>
+        <button type='submit'>{action === 'Sign Up' ? "Login" : "Sign up"}</button>
+        <div className={action === 'Sign Up' ? "signup" : "login"}>
+          <p>{action === 'Sign Up' ? "Don't have an account" : "Already have an account?"}
+            <a href='#' onClick={() => { action === 'Sign Up' ? setAction("Log in") : setAction("Sign Up") }}>{action}</a>
+          </p>
+        </div>
+      </form>
     </div>
   )
 }
